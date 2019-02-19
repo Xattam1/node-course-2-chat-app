cf4103b8bde285857da99f7d0144c6b4d623b910
Justin <jgran1@unh.newhaven.edu>
2019-02-19 12:47:23 -0500

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,22 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	// socket.emit('newMessage', {
+	// 	from: 'John',
+	// 	text: 'See you then',
+	// 	createdAt: Date.now()
+	// });
+
+	socket.on('createMessage', (message) => {
+		console.log('createMessage', message);
+
+		socket.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: Date.now()
+		});
+	});
+
 	socket.on('disconnect', () => {
 		console.log('User was disconnected');
 	});
